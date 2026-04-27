d6414920591c3ac15437730566d3cb5b9879d460
Fares <fares.ayman.soliman2006@gmail.com>
2026-04-27 20:38:19 +0300

    Fixed error in preprocess_resume_dataframe func

--- src/resume_pipeline/preprocessing.py
+++ src/resume_pipeline/preprocessing.py
@@ -207,7 +207,7 @@ def preprocess_resume_dataframe(
 
     processed_df = df.copy()
 
-    processed_df[output_column] = preprocess_resume_dataframe(
+    processed_df[output_column] = preprocess_text_series(
         text_series = processed_df[text_column],
         remove_stopwords= remove_stopwords,
         extra_stopwords= extra_stopwords
